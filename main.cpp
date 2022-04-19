9c7ef5cdaaf451f4a0b58a987655899b97ba1d30
DigitalConcern <mihanik1608@gmail.com>
2022-04-19 17:00:31 +0300

    Init

---
+++ main.cpp
@@ -0,0 +1,19 @@
+<<<<<<< HEAD
+#include "mainwindow.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+=======
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+>>>>>>> 73e7cc13ed538410ec51f1524e044182c10cd938
+}
